1c5f510f6e3c608d3bd97dfdb269df60702bb98b
Oumayma AMRI <Oumayma.AMRI@Teal.ma>
2021-12-20 18:10:06 +0100

    Add project files.

---
+++ EncryptPwd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EncryptPwd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please enter a string to encrypt:");
+            string plaintext = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("Your encrypted string is:");
+            string encryptedstring = StringCipher.EnryptString(plaintext);
+            Console.WriteLine(encryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Your decrypted string is:");
+            string decryptedstring = StringCipher.DecryptString(encryptedstring);
+            Console.WriteLine(decryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
